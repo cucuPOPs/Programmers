ccf3b108e100d9a8c9ae8417d672bc12c5c8558b
박기락 <kirak0622@naver.com>
2021-12-11 15:16:45 +0900

    110 solve

--- Programmers/Programmers/_100/_110.cs
+++ Programmers/Programmers/_100/_110.cs
@@ -1,16 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //제일 작은 수 제거하기
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021.12.11
+        //ez
         public void Solution_110()
         {
             //
+            foreach (var t in Solution(new int[]{4,3,2,1}))
+            {
+                Console.WriteLine($"{t},");//4,3,2    
+            }
+
+            foreach (var t in Solution(new int[]{10}))
+            {
+                Console.WriteLine($"{t},");//-1  
+            }
+            
+            int[] Solution(int[] arr)
+            {
+                if (arr.Length == 1) return new int[] {-1};
+                 
+                int min = arr.ToList().Min();
+                return arr.ToList().Where(t => t != min).ToArray();
+            }
         }
     }
 }
