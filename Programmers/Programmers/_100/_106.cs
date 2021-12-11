a6e6e7370aebb24d54dea5463828e1d4dba49819
박기락 <kirak0622@naver.com>
2021-12-11 13:08:32 +0900

    106 solve

--- Programmers/Programmers/_100/_106.cs
+++ Programmers/Programmers/_100/_106.cs
@@ -1,16 +1,24 @@
 ﻿using System;
-
+using System.Linq;
 namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //평균 구하기.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021.12.11.
+        //linq 함수가 싱기하네에에
         public void Solution_106()
         {
-            //
+            Console.WriteLine(Solution(new int[] {1, 2, 3, 4}));
+            
+            double Solution(int[] arr) {
+                return (double)arr.ToList().Sum() / arr.Length;
+            }
+            
+            double Solution_Average(int[] arr) {
+                return arr.Average();
+            }
         }
     }
 }
