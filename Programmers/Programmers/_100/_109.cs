c744c02ca38959c1d8a534d966413ea73d7ee07c
박기락 <kirak0622@naver.com>
2021-12-11 15:04:11 +0900

    109 solve

--- Programmers/Programmers/_100/_109.cs
+++ Programmers/Programmers/_100/_109.cs
@@ -4,13 +4,18 @@ namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //짝수와 홀수
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021.12.11
+        //ez
         public void Solution_109()
         {
-            //
+            Console.WriteLine(Solution(3));//"Odd"
+            Console.WriteLine(Solution(4));//"Even"
+            string Solution(int num)
+            {
+                return num % 2 == 0 ? "Even" : "Odd";
+            }
         }
     }
 }
