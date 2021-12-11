53f9e15a013d40077bd46de4abab5e3e4a0b1c33
박기락 <kirak0622@naver.com>
2021-12-11 14:18:26 +0900

    107 solve

--- Programmers/Programmers/_100/_107.cs
+++ Programmers/Programmers/_100/_107.cs
@@ -4,13 +4,49 @@ namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //콜라츠 추측.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021.12.11.
+        //정답만 맞추지말고 코드에도 신경을 써보자...
         public void Solution_107()
         {
+            Console.WriteLine(Solution(6));//8
+            Console.WriteLine(Solution(16));//4
+            Console.WriteLine(Solution(626331));//-1
             //
+            int Solution(int num)
+            {
+                long newNum = num;
+                int count = 0;
+                while (count < 500)
+                {
+                    if (newNum == 1) break;
+                    if (newNum % 2 == 0)
+                    {
+                        newNum /= 2;
+                    }
+                    else
+                    {
+                        newNum=newNum * 3 + 1;
+                    }
+
+                    count++;
+                }
+
+                if (count == 500)
+                    return -1;
+                return count;
+            }
+            
+            int solution_useFor(int num) {
+                long lNum = num;//long num 이 네이밍이 더 좋네.
+                for (int i = 0; i < 500; i++)
+                {
+                    if (lNum == 1) return i;
+                    lNum = lNum % 2 == 0 ? lNum / 2 : lNum * 3 + 1;                        
+                }
+                return -1;        
+            }
         }
     }
 }
