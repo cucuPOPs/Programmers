875db2c98b93d44d22ad0d9912fa25adb3182813
박기락 <kirak0622@naver.com>
2021-12-11 16:26:12 +0900

    202 solved

--- Programmers/Programmers/_200/_202.cs
+++ Programmers/Programmers/_200/_202.cs
@@ -1,15 +1,32 @@
 ﻿using System;
+using System.Linq;
+
 namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //JadenCase 문자열 만들기.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021.12.11.
+        //나름 깔끔하게 짠거같음 ㅎ 뿌듯잼ㅋ
         public void Solution_202()
         {
+            Console.WriteLine(Solution("aaaaa    aaa"));//"Aaaaa    Aaa"
+            Console.WriteLine(Solution("3people unFollowed me"));//"3people Unfollowed Me"
+            Console.WriteLine(Solution("for the last week"));//"For The Last Week"
             //
+            string Solution(string s)
+            {
+                string[] t = s.Split(' ');
+                for (int i = 0; i < t.Length; i++)
+                {
+                    if(string.IsNullOrEmpty(t[i])) continue;
+                    t[i] = t[i].ToUpper().First() + t[i].ToLower().Substring(1);
+                }
+
+                return string.Join(' ',t);
+
+            }
         }
     }
 }
