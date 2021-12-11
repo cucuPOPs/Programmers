c5399f666dedec216a483b3b135d22c69e3f9845
박기락 <kirak0622@naver.com>
2021-12-11 10:31:03 +0900

    101 solve & rule definition
    문제 템플릿? 작성.

---
+++ Programmers/Programmers/_101.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Programmers
+{
+    public partial class Program
+    {
+        //직사각형 별찍기
+        //++++++++++++++++++++++++++
+        //2021.12.11
+        //너무 easy했다
+        public void Solution_101()
+        {
+            String[] s;
+            Console.Clear();
+            s = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(s[0]);
+            int b = int.Parse(s[1]);
+
+            for(int i=0;i<b;i++)
+            {
+                for(int j=0;j<a;j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
