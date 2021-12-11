90a7f8e5a6a2cbcc4711138268fa8a5aa710d33e
박기락 <kirak0622@naver.com>
2021-12-11 15:39:24 +0900

    201 solve

--- Programmers/Programmers/_200/_201.cs
+++ Programmers/Programmers/_200/_201.cs
@@ -1,15 +1,40 @@
 ﻿using System;
+
 namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //N개의 최소공배수.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021.12.11.
+        //108문제 응용.
+        //ez
         public void Solution_201()
         {
             //
+            Console.WriteLine(Solution(new int[]{2,6,8,14}));//168
+            Console.WriteLine(Solution(new int[]{1,2,3}));//6
+            //
+            int Solution(int[] arr)
+            {
+                int answer = arr[0];
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    answer = lcm(answer, arr[i]);
+                }
+
+                return answer;
+            }
+
+            int gcd(int a, int b)
+            {
+                return a % b == 0 ? b : gcd(b, a % b);
+            }
+
+            int lcm(int a, int b)
+            {
+                return a * b / gcd(a, b);
+            }
         }
     }
 }
