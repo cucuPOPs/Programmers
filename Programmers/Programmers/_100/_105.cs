b35e8de056c137da824801e68c9e6b97dd91b7c4
박기락 <kirak0622@naver.com>
2021-12-11 13:01:00 +0900

    105 solve

--- Programmers/Programmers/_100/_105.cs
+++ Programmers/Programmers/_100/_105.cs
@@ -1,16 +1,42 @@
 ﻿using System;
-
+using System.Linq;
 namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //하샤드 수.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021.12.11.
+        //linq...한줄코딩 멋지네
         public void Solution_105()
         {
-            //
+            Console.WriteLine(solution(10));
+            Console.WriteLine(solution(12));
+            Console.WriteLine(solution(11));
+            Console.WriteLine(solution(13));
+            bool solution(int x) {
+                int haShad = 0;
+                int origin = x;
+                int max = x.ToString().Length;
+                for (int i = 0; i < max; i++)
+                {
+                    haShad += x % 10;
+                    x /= 10;
+                }
+
+                return origin % haShad == 0;
+            }
+            
+            bool solution_useLinq(int x) {
+                bool answer = true;
+
+                var temp = x.ToString().ToList().Select(y => int.Parse(y.ToString())).Sum();
+
+                if (x % temp != 0)
+                    answer = false;
+
+                return answer;
+            }
         }
     }
 }
