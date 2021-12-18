85ca7189bc3b66fdd40dfc1462de65c337ec5e00
박기락 <kirak0622@naver.com>
2021-12-19 05:40:31 +0900

    304 solve

--- Programmers/Programmers/_300/_304.cs
+++ Programmers/Programmers/_300/_304.cs
@@ -1,15 +1,33 @@
 ﻿using System;
+
 namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //멀리 뛰기.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021.12.19.
+        //1칸 = [1]
+        //2칸 = [1,1] [2]
+        //3칸 = [1,1,1] [2,1] [1,2]
+        //4칸 = [1,1,1,1] [2,1,1] [1,2,1] [1,1,2] [2,2]
+        //N칸 = [N-1칸 foreach] + [1] , [N-2칸 foreach]+[2]
+
         public void Solution_304()
         {
-            //
+            long Solution(int n)
+            {
+                int one_step = 1;
+                int two_step = 2;
+                if (n == 1)
+                    return 1;
+                for (int i = 2; i < n; i++)
+                {
+                    (one_step, two_step) = (two_step, (one_step + two_step) % 1234567);
+                }
+
+                return two_step % 1234567;
+            }
         }
     }
 }
