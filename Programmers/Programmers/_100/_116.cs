bd14014da52dd6073987ef823d3008f0fcdf8425
박기락 <kirak0622@naver.com>
2021-12-20 06:45:42 +0900

    116,117 solve

--- Programmers/Programmers/_100/_116.cs
+++ Programmers/Programmers/_100/_116.cs
@@ -4,13 +4,28 @@ namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //약수의 합.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021.12.20.
+        //ez.
+        //short code 보다 가독성!!!
         public void Solution_116()
         {
-            //
+            Solution(12);//28 ::1+2+3+4+6+12 = 28
+            Solution(5);//6 ::1+5=6
+            int Solution(int n) {
+                int answer = 0;
+                for (int i = 1; i * i <= n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        answer += i;
+                        //25 : 1 5 5 25=> 1 5 25
+                        if (i * i != n)answer += n / i;
+                    }
+                }
+                return answer;
+            }
         }
     }
 }
