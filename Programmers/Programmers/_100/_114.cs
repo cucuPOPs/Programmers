c358a9d98a5d76e9459e80ff1fd1da1c5854cf6c
박기락 <kirak0622@naver.com>
2021-12-20 05:31:15 +0900

    114 solve

--- Programmers/Programmers/_100/_114.cs
+++ Programmers/Programmers/_100/_114.cs
@@ -1,16 +1,30 @@
 ﻿using System;
+using System.Linq;
 
 namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //자릿수 더하기.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021.12.20.
+        //ez.
         public void Solution_114()
         {
-            //
+            //1+2+3=6
+            Solution(123);//6
+
+            int Solution(int n)
+            {
+                int result = 0;
+                while (n > 0)
+                {
+                    result += n % 10;
+                    n /= 10;
+                }
+
+                return result;
+            }
         }
     }
 }
