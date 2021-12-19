3ec19ef90f2484a9ee921bf3ae491f3c110b8ae2
박기락 <kirak0622@naver.com>
2021-12-20 07:25:08 +0900

    118, 119, 120 solve

--- Programmers/Programmers/_100/_118.cs
+++ Programmers/Programmers/_100/_118.cs
@@ -4,13 +4,19 @@ namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //문자열을 정수로 바꾸기.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021.12.20.
+        //한줄코딩잼.ez
         public void Solution_118()
         {
-            //
+            Solution("1234");//1234
+            Solution("+1234");//1234
+            Solution("-1234");//1234
+            int Solution(string s)
+            {
+                return int.Parse(s);
+            }
         }
     }
 }
--- Programmers/Programmers/_100/_120.cs
+++ Programmers/Programmers/_100/_120.cs
@@ -1,16 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //소수 찾기.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021.12.20.
+        //ez.제곱근.
         public void Solution_120()
         {
-            //
+            int test_1 = Solution(10);//2,3,5,7 = 4개
+            Console.WriteLine(test_1);//4
+            
+            int test_2 = Solution(5);//2,3,5 = 3개
+            Console.WriteLine(test_2);//3
+            
+            int Solution(int n)
+            {
+                int cnt = 0;
+                for (int i = 2; i <= n; i++)
+                {
+                    if (IsPrime(i))
+                    {
+                        cnt++;
+                    }    
+                }
+                return cnt;
+            }
+        }
+        public bool IsPrime(int n)
+        {
+            for (int i = 2; i*i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
