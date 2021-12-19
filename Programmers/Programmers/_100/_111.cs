70e79523f7d4c892d00ddf9aa721a2c7de7989d0
박기락 <kirak0622@naver.com>
2021-12-20 04:13:03 +0900

    111 solve

--- Programmers/Programmers/_100/_111.cs
+++ Programmers/Programmers/_100/_111.cs
@@ -1,16 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //정수 제곱근 판별.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021.12.20.
+        // if (sqrt % 1.0 != 0) 배워갑니다
         public void Solution_111()
         {
-            //
+            Console.WriteLine(Solution(121));//144
+            Console.WriteLine(Solution(3));//-1
+            long Solution(long n)
+            {
+                for (long i = 1; i * i <= n; i++)
+                {
+                    if ( i *i == n)
+                    {
+                        return (i + 1) * (i + 1);
+                    }
+                }
+                return -1;
+            }
+        }
+
+        private long Solution_UseMath(long n)
+        {
+            long answer = 0;
+
+            var sqrt = Math.Sqrt(n);
+
+            //Wow...정수판정을 이렇게!!
+            if (sqrt % 1.0 != 0) return -1;
+
+            answer = (long) sqrt;
+
+            answer += 1;
+            answer *= answer;
+
+            return answer;
         }
     }
 }
