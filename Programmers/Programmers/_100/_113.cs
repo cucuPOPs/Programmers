bac8ca1d3057a91a2dd3c251cdf33a7e5c8de0d5
박기락 <kirak0622@naver.com>
2021-12-20 05:26:56 +0900

    113 solve

--- Programmers/Programmers/_100/_113.cs
+++ Programmers/Programmers/_100/_113.cs
@@ -1,16 +1,22 @@
 ﻿using System;
+using System.Linq;
 
 namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //자연수 뒤집어 배열로 만들기.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021.12.20.
+        //Short code를 위해, 검색함 ㅎ.
         public void Solution_113()
         {
-            //
+            int[] Solution(long n)
+            {
+                char[] a = n.ToString().ToCharArray();
+                Array.Reverse(a);
+                return Array.ConvertAll(a,c=> (int) Char.GetNumericValue(c));
+            }
         }
     }
 }
