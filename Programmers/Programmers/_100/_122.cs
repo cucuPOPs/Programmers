93fb5865d455eab2eaf3296a9bc701575dd97931
박기락 <kirak0622@naver.com>
2021-12-22 11:35:00 +0900

    122 solve

--- Programmers/Programmers/_100/_122.cs
+++ Programmers/Programmers/_100/_122.cs
@@ -1,16 +1,37 @@
 ﻿using System;
-
+using System.Linq;
 namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //문자열 다루기 기본.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021.12.22.
+        //? very ez.
+        //tryParse도 있구나..
         public void Solution_122()
         {
-            //
+            Console.WriteLine(Solution("a234"));//false
+            Console.WriteLine(Solution("1234"));//false
+            
+            bool Solution(string s)
+            {
+                if (s.Length != 4 && s.Length != 6) return false;
+                char[] ch = s.ToCharArray();
+                foreach (char c in ch)
+                {
+                    if (!char.IsNumber(c)) return false;
+                }
+                return true;
+            }
+        }
+
+        bool Solution_parse(string s)
+        {
+            bool answer = false;
+            if (s.Length == 4 || s.Length == 6)            
+                answer = int.TryParse(s, out int i);            
+            return answer;
         }
     }
 }
