f6308133b76868424ffe7da7a754f06c85c631f8
박기락 <kirak0622@naver.com>
2021-12-22 12:05:43 +0900

    123 solve

--- Programmers/Programmers/_100/_123.cs
+++ Programmers/Programmers/_100/_123.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-
+using System.Linq;
 namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //문자열 내림차순으로 배치하기.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021.12.22.
+        //ez
         public void Solution_123()
         {
-            //
+            string s = Solution("Zbcdefg");
+            Console.WriteLine(s);
+            string Solution(string s)
+            {
+                char[] ch = s.ToCharArray();
+                Array.Sort(ch,(a,b)=>b.CompareTo(a));
+                return new string(ch);
+            }
         }
     }
 }
