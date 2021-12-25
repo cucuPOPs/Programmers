0b5c33bdc07397c7deecb1fa078893a1304bf10e
박기락 <kirak0622@naver.com>
2021-12-25 17:02:48 +0900

    126 solve

--- Programmers/Programmers/_100/_126.cs
+++ Programmers/Programmers/_100/_126.cs
@@ -1,16 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //나누어 떨어지는 숫자 배열.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021-12-25.
+        //ez.
         public void Solution_126()
         {
+            int[] result = solution(new int[] {5, 9, 7, 10}, 5);
+            foreach (int i in result)
+            {
+                Console.Write($"{i} ,");
+            }
+
             //
+            int[] solution(int[] arr, int divisor)
+            {
+                int[] answer = arr.Where(t => t % divisor == 0).OrderBy(x => x).ToArray();
+                return answer.Length == 0 ? new int[] {-1} : answer;
+            }
         }
     }
 }
