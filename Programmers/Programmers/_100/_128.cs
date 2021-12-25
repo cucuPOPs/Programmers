4c18333d7d38884843ba5e8f40b7970fa6b6176f
박기락 <kirak0622@naver.com>
2021-12-25 17:27:54 +0900

    128 solve

--- Programmers/Programmers/_100/_128.cs
+++ Programmers/Programmers/_100/_128.cs
@@ -4,13 +4,24 @@ namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //부족한 금액 계산하기.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021-12-25.
+        //return Math.Max(0, answer - money);
+        //가독성 짱...wow.
+        //등차수열의 합 : 평균 x 개수
         public void Solution_128()
         {
+            long result = solution(3, 20, 4);
+            Console.WriteLine(result);
             //
+            long solution(int price, int money, int count)
+            {
+                long answer = count * (price + price * count) / 2;
+                return answer - money < 0 ? 0 : answer - money;
+                //wow.
+                //return Math.Max(0, answer - money);
+            }
         }
     }
 }
