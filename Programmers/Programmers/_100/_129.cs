8375088ab238d94ec525a45f64288fb37c3ade99
박기락 <kirak0622@naver.com>
2021-12-25 17:40:06 +0900

    129 solve

--- Programmers/Programmers/_100/_129.cs
+++ Programmers/Programmers/_100/_129.cs
@@ -4,13 +4,28 @@ namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //나머지가 1이 되는 수 찾기.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021-12-25.
+        //ez...
         public void Solution_129()
         {
+            solution(10);//3
+            solution(12);//11
             //
+            int solution(int n)
+            {
+                int answer = 0;
+                for (int i = 1 ; i < n; i++)
+                {
+                    if (n % i == 1)
+                    {
+                        answer = i;
+                        break;
+                    }
+                }
+                return answer;
+            }
         }
     }
 }
