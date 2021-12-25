d1453f6d6a71a84436bea9a618e7f8dae2f5ba7d
박기락 <kirak0622@naver.com>
2021-12-25 17:09:06 +0900

    127 solve

--- Programmers/Programmers/_100/_127.cs
+++ Programmers/Programmers/_100/_127.cs
@@ -4,13 +4,22 @@ namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //가운데 글자 가져오기.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2021-12-25.
+        //ez.
         public void Solution_127()
         {
+            string t1 =solution("abcde");
+            string t2 = solution("qwer");
+            Console.WriteLine(t1);//c
+            Console.WriteLine(t2);//we
+            
             //
+            string solution(string s)
+            {
+                return s.Length % 2 == 0 ? s.Substring((s.Length-1)/2, 2) : s.Substring(s.Length/2, 1);
+            }
         }
     }
 }
