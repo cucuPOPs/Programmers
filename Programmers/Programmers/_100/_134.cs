8e8abbf79a8620fadd7953113cc742ee1c14f0a0
박기락 <kirak0622@naver.com>
2022-01-09 18:14:17 +0900

    134 solve

--- Programmers/Programmers/_100/_134.cs
+++ Programmers/Programmers/_100/_134.cs
@@ -1,16 +1,26 @@
 ﻿using System;
+using System.Linq;
 
 namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //음양 더하기.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2022.01.09.
+        //한줄wow..
+        //return absolutes.Select((v, i) => signs[i] == true ? v : -v).Sum();
         public void Solution_134()
         {
-            //
+            int solution(int[] absolutes, bool[] signs)
+            {
+                long result = 0;
+                for (int i = 0; i < absolutes.Length; i++)
+                {
+                    result += (signs[i] == true ? 1 : -1) * absolutes[i];
+                }
+                return (int)result;
+            }
         }
     }
 }
