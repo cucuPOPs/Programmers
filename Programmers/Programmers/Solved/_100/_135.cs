73fe471d2a303615d6e7a9b0fb57ca5670715976
박기락 <kirak0622@naver.com>
2022-01-09 18:33:20 +0900

    135 solve

---
+++ Programmers/Programmers/Solved/_100/_135.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Programmers
+{
+    public partial class Program
+    {
+        //없는 숫자 더하기.
+        //++++++++++++++++++++++++++
+        //2022.01.09.
+        //ez.
+        public void Solution_135()
+        {
+            //
+            Console.WriteLine(solution(new int[]{1,2,3}));
+            int solution(int[] numbers)
+            {
+                return 55 - numbers.Sum();
+            }
+        }
+    }
+}
