bdee904f1ef6f1a66ce7af112032fa76629d569e
박기락 <kirak0622@naver.com>
2022-01-11 03:36:01 +0900

    139 solve 어우잠온다

---
+++ Programmers/Programmers/Solved/_100/_139.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+namespace Programmers
+{
+    public partial class Program
+    {
+        //로또의 최고 순위와 최저 순위.
+        //++++++++++++++++++++++++++
+        //2022.01.11.
+        //C# 신기능 스위치문은 안되네 ㅎ. 어우 잠온다
+        public void Solution_139()
+        {
+            int[] r =solution(new int[] {44, 1, 0, 0, 31, 25}, new int[] {31, 10, 45, 1, 6, 19});
+            foreach (var i in r)
+            {
+                Console.WriteLine($"{i}, ");
+            }
+            //
+            int[] solution(int[] lottos, int[] win_nums) {
+               int invisibleCount = lottos.Count(t => t==0);
+               int matchedCount = lottos.Count(win_nums.Contains);
+               int maxMatch = matchedCount + invisibleCount;
+               int minMatch = matchedCount;
+               int bestRank, worstRank;
+               switch (maxMatch)
+               {
+                   case 6: bestRank = 1;break;
+                   case 5: bestRank = 2;break;
+                   case 4: bestRank = 3;break;
+                   case 3: bestRank = 4;break;
+                   case 2: bestRank = 5;break;
+                   default: bestRank = 6; break;
+               }
+               switch (minMatch)
+               {
+                   case 6: worstRank = 1;break;
+                   case 5: worstRank = 2;break;
+                   case 4: worstRank = 3;break;
+                   case 3: worstRank = 4;break;
+                   case 2: worstRank = 5;break;
+                   default: worstRank = 6; break;
+               }
+               // int bestRank =  maxMatch switch
+               // {
+               //     6=>1, 5=>2, 4=>3, 3=>4, 2=>5, _=>6
+               // };
+               // int worstRank =  minMatch switch
+               // {
+               //     6=>1, 5=>2, 4=>3, 3=>4, 2=>5, _=>6
+               // };
+               return new int[] {bestRank, worstRank};
+            }
+        }
+    }
+}
