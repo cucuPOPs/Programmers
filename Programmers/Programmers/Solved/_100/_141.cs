71db9c07f4fd37c6456d968c860490f2bad10191
박기락 <kirak0622@naver.com>
2022-01-16 16:07:56 +0900

    141 solve

---
+++ Programmers/Programmers/Solved/_100/_141.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Programmers
+{
+    public partial class Program
+    {
+        //3진법 뒤집기.
+        //++++++++++++++++++++++++++
+        //2022.01.16.
+        //와우....초깔끔...
+        //hashTag; #진수변환 #진법변환
+        public void Solution_141()
+        {
+            int result = solution(45);
+            Console.WriteLine(result);//7
+            int solution(int n)
+            {
+                List<int> list = new List<int>();
+                while (n != 0)
+                {
+                    list.Add(n % 3);
+                    n /= 3;
+                }
+
+                list.Reverse();
+
+                return (int)list.Select((x, idx) => x * Math.Pow(3, idx)).Sum();
+            }
+        }
+    }
+    
+}
+
