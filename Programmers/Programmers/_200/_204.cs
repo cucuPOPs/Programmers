8b5b529a7d76385af01b6a276cf1417e081a894b
박기락 <kirak0622@gmail.com>
2022-08-14 22:58:47 +0900

    solve 204

--- Programmers/Programmers/_200/_204.cs
+++ Programmers/Programmers/_200/_204.cs
@@ -1,15 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Programmers
 {
     public partial class Program
     {
-        //문제이름.
+        //기능개발.
         //++++++++++++++++++++++++++
-        //작성날짜.
-        //느낀점.
+        //2022.08.14.
+        //흠...
         public void Solution_204()
         {
-            //
+            var result = solution(new int[] {93, 30, 55}, new int[] {1, 30, 5});
+            Console.WriteLine(result);
+            
+            
+            int[] solution(int[] progresses, int[] speeds)
+            {
+                int day = 0;
+                int max_day = 0;
+                
+                var result = new List<int>();
+                for (int i = 0; i < progresses.Length; ++i)
+                {
+                    day = GetDay(progresses[i], speeds[i]);
+                    if (max_day < day)
+                    {
+                        result.Add(1);    
+                    }
+                    else
+                    {
+                        result[^1]++;
+                    }
+                    
+                    if (max_day < day)
+                    {
+                        max_day = day;
+                    }
+                }
+
+                return result.ToArray();
+            }
+
+            int GetDay(int progress, int speed)
+            {
+                return (int) Math.Ceiling((100f - progress) / speed);
+            }
+                
+
+
         }
     }
 }
