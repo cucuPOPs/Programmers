0b1915fd86405209315e2d54ffb31c7bc263e9b9
박기락 <kirak0622@gmail.com>
2024-02-23 20:29:55 +0900

    .

---
+++ 바킹독/0x07덱/덱.cpp
@@ -0,0 +1,61 @@
+﻿// 10866번: 덱
+#include <bits/stdc++.h>
+using namespace std;
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	deque<int> dq;
+	int n;
+	cin >> n;
+	while (n--)
+	{
+		string op;
+		cin >> op;
+		if (op == "push_back")
+		{
+			int t;
+			cin >> t;
+			dq.push_back(t);
+		}
+		else if (op == "push_front")
+		{
+			int t;
+			cin >> t;
+			dq.push_front(t);
+		}
+		else if (op == "pop_front")
+		{
+			if (dq.empty()) cout << -1 << '\n';
+			else
+			{
+				cout << dq.front() << '\n';
+				dq.pop_front();
+			}
+		}
+		else if (op == "pop_back")
+		{
+			if (dq.empty()) cout << -1 << '\n';
+			else
+			{
+				cout << dq.back() << '\n';
+				dq.pop_back();
+			}
+		}
+		else if (op == "size")
+			cout << dq.size() << '\n';
+		else if (op == "empty")
+			cout << dq.empty() << '\n';
+		else if (op == "front")
+		{
+			if (dq.empty()) cout << -1 << '\n';
+			else cout << dq.front() << '\n';
+		}
+		else if (op == "back")
+		{
+			if (dq.empty()) cout << -1 << '\n';
+			else cout << dq.back() << '\n';
+		}
+	}
+}
