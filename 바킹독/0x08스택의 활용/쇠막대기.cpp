6f35e497ae2dabbb37f0f0dbb1354a9e77b6b5ac
박기락 <kirak0622@gmail.com>
2024-02-27 23:03:02 +0900

    .

---
+++ 바킹독/0x08스택의 활용/쇠막대기.cpp
@@ -0,0 +1,26 @@
+﻿// 10799번: 쇠막대기
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string str;
+	cin >> str;
+
+	int count = 0;
+	int floor = 0;
+	int len = (int)str.size();
+	for (int i = 0; i < len; ++i)
+	{
+		if (str[i] == '(') ++floor;
+		else
+		{
+			--floor;
+			if (str[i - 1] == '(') count += floor;
+			else count++;
+		}
+	}
+	cout << count;
+}
