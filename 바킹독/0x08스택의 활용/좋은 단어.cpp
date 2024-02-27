5e6ea7a6e3478928a12c39f97c693c3091ff2b37
박기락 <kirak0622@gmail.com>
2024-02-27 21:47:41 +0900

    .

---
+++ 바킹독/0x08스택의 활용/좋은 단어.cpp
@@ -0,0 +1,27 @@
+﻿// 3986번: 좋은 단어
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	int count = 0;
+	while (n--)
+	{
+		string str;
+		cin >> str;
+
+		stack<char> stack;
+		int len = (int)str.size();
+		for (int j = 0; j < len; j++)
+		{
+			if (!stack.empty() && stack.top() == str[j]) stack.pop();
+			else stack.push(str[j]);
+		}
+		if (stack.empty()) count++;
+	}
+	cout << count;
+}
