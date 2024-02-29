eda909052d7d5b9869b1829ff3be86d676d80c67
박기락 <kirak0622@gmail.com>
2024-02-29 22:52:16 +0900

    .

---
+++ 바킹독/0x08BFS/숨바꼭질.cpp
@@ -0,0 +1,38 @@
+﻿// 1697번: 숨바꼭질
+#include <bits/stdc++.h>
+using namespace std;
+int n, k;
+int dist[100001];
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> k;
+
+    for (int i = 0; i < 100001; i++)
+    {
+        dist[i] = -1;
+    }
+
+    queue<int> Q;
+    Q.push(n);
+    dist[n] = 0;
+
+    while (!Q.empty())
+    {
+        int cur = Q.front();
+        Q.pop();
+        for (int nx : {cur - 1, cur + 1, 2 * cur})
+        {
+            if (nx < 0 || nx > 100000) continue; // out range 방어.
+            if (dist[nx] > -1) continue;         // 재방문 방어.
+            dist[nx] = dist[cur] + 1;
+            Q.push(nx);
+        }
+    }
+
+    cout << dist[k];
+}
+
+// 신기한 for range문.
