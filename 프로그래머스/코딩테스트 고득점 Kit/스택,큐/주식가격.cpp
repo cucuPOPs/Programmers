6d0c9170378c520e4bf2a2226f33486de846fd6a
박기락 <kirak0622@gmail.com>
2024-03-13 16:09:58 +0900

    .

---
+++ 프로그래머스/코딩테스트 고득점 Kit/스택,큐/주식가격.cpp
@@ -0,0 +1,16 @@
+// 주식가격
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> solution(vector<int> prices)
+{
+    vector<int> answer(prices.size(), 0);
+    for (int i = 0; i < prices.size() - 1; i++)
+    {
+        for (int j = i + 1; j < prices.size(); j++)
+        {
+            answer[i]++;
+            if (prices[i] > prices[j]) break;
+        }
+    }
+    return answer;
+}
