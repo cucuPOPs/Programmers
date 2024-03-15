7e98a6df97899c8aa1615c488a06922fbd71e7f5
박기락 <kirak0622@gmail.com>
2024-03-15 15:31:48 +0900

    .

---
+++ 바킹독/0x12수학/소인수분해.cpp
@@ -0,0 +1,21 @@
+// 11653번: 소인수분해
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    for (int i = 2; i * i <= n; i++)
+    {
+        while (n % i == 0)
+        {
+            cout << i << '\n';
+            n /= i;
+        }
+    }
+    if (n != 1) cout << n;
+}
